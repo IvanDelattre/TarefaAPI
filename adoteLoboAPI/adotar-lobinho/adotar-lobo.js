b8cca384a54d10725aef5d90010e62de65fd7900
ivanc <ivandelattre7@gmail.com>
2025-02-18 13:46:22 -0300

    feat: adotar-lobo

--- adoteLoboAPI/adotar-lobinho/adotar-lobo.js
+++ adoteLoboAPI/adotar-lobinho/adotar-lobo.js
@@ -1,5 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let loboSelecionado = JSON.parse(localStorage.getItem("loboSelecionado"));
+document.addEventListener("DOMContentLoaded", async function () {
+    //let loboSelecionado = JSON.parse(localStorage.getItem("loboSelecionado"));
+    let list_lobo;
+    try{
+        const response =  await fetch("http://localhost:3000/loboSelecionado/", {
+            method: "GET"
+        })
+        if ( !response.ok  ){
+            throw new Error("error") ; 
+        }
+        list_lobo = await response.json() ;
+    }catch(error ) {
+        
+        console.log(error)
+
+    }
+    
+
+    console.log(list_lobo) ; 
+
+
+    let num = list_lobo.length - 1 ; 
+    let loboSelecionado = list_lobo[num] ; 
+
+    
+
+    
+
+
     if (!loboSelecionado) {
         document.body.innerHTML = "<h1>Lobo não encontrado!</h1>";
         return;
@@ -12,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Captura o botão de adoção
     let botaoAdotar = document.querySelector("#button");
-    botaoAdotar.addEventListener("click", function (event) {
+    botaoAdotar.addEventListener("click", async function (event) {
         event.preventDefault();
         
         let nomeDono = document.querySelector("#donoNome").value.trim();
@@ -31,7 +58,29 @@ document.addEventListener("DOMContentLoaded", function () {
         loboSelecionado.emailDono = emailDono;
 
         // Atualiza o localStorage
-        localStorage.setItem("loboSelecionado", JSON.stringify(loboSelecionado));
+        //localStorage.setItem("loboSelecionado", JSON.stringify(loboSelecionado));
+
+        let data = "http://localhost:3000/lobos/"
+        data += loboSelecionado.id ;
+        
+        await fetch(data ,{
+            method : 'PATCH',
+            headers : {"Content-type" : "application/json"},
+            body : JSON.stringify( loboSelecionado ) 
+        })
+        
+        /*
+        data = "http://localhost:3000/loboSelecionado/"
+        data += loboSelecionado.id ;
+        
+        await fetch(data ,{
+            method : 'PATCH',
+            headers : {"Content-type" : "application/json"},
+            body : JSON.stringify( loboSelecionado ) 
+        })
+        */
+
+
         
         let lobos = JSON.parse(localStorage.getItem("lobos")) || [];
         lobos = lobos.map(lobo => (lobo.id === loboSelecionado.id ? loboSelecionado : lobo));
